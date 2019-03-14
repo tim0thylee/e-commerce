04b432995cbf59b9be6da3173f5b4a10646b1a86
Timothy Lee <31238530+tim0thylee@users.noreply.github.com>
2019-03-14 00:22:11 -0700

    fix merg bugs

--- e-commerce/src/api/unsplash.js
+++ e-commerce/src/api/unsplash.js
@@ -4,10 +4,6 @@ import {API_KEY} from './keys'
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-<<<<<<< HEAD
         Authorization: `Client-ID ${API_KEY}`
-=======
-        Authorization: 'Client-ID'
->>>>>>> 8c8999014fb72f2681d36cdb60c680b1a7b92559
     }
 })
